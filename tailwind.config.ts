b80e2ae426fd10021b846b3599a1cd7f643994ca
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-17 02:22:03 +0300

    feat: Update Tailwind CSS configuration with new theme colors and grid templates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,41 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import typography from "@tailwindcss/typography"
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx,mdx}"
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "spiral": "url('/assets/design/ssspiral.svg')"
       },
-    },
+      colors: {
+        primary: {
+          50: "#fef2f4",
+          100: "#fde6e9",
+          200: "#fbd0d9",
+          300: "#f7aab9",
+          400: "#f27a93",
+          500: "#e63f66",
+          600: "#d42a5b",
+          700: "#b21e4b",
+          800: "#951c45",
+          900: "#801b40",
+          950: "#470a1f"
+        }
+      },
+      gridTemplateRows: {
+        "dashboard": "auto 1fr",
+      },
+      gridTemplateColumns: {
+        "dashboard": "20rem 1fr",
+        "cards": "repeat(auto-fill, minmax(16rem, 1fr))"
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [typography]
+}
+
+export default config
