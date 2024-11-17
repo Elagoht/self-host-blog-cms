ced0b9ca6cf871a5727883c89e8443b58f18119e
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-17 13:42:09 +0300

    feat: Markdown-it integration with various plugins for enhanced rendering

---
+++ src/lib/markdown/markdown-it.ts
@@ -0,0 +1,57 @@
+import hljs from "highlight.js"
+import MarkdownIt from "markdown-it"
+import abbr from "markdown-it-abbr"
+import anchor from "markdown-it-anchor"
+import deflist from "markdown-it-deflist"
+import expandable from "markdown-it-expandable"
+import footnote from "markdown-it-footnote"
+import iframe from "markdown-it-iframe"
+import ins from "markdown-it-ins"
+import mark from "markdown-it-mark"
+import smartarrows from "markdown-it-smartarrows"
+import sub from "markdown-it-sub"
+import sup from "markdown-it-sup"
+import task from "markdown-it-task-lists"
+import emoji from "markdown-it-emoji"
+import { escapeHtml } from "markdown-it/lib/common/utils.mjs"
+
+const convertMarkdownToHtml = (content: string): string => {
+  const converter = new MarkdownIt({
+    highlight: function (str, lang) {
+      const begin = `<div class="code-container"><div class="code-header"><span class="code-lang">${lang}</span><button class="code-copy" onclick="navigator.clipboard.writeText(\`${escapeHtml(str).replace(/`/g, "\\`").replace(/\$/g, "\\$")}\`)">📋</button></div><div class="hljs">`
+      const end = "</div></div>"
+
+      if (lang && hljs.getLanguage(lang))
+        try {
+          return begin + hljs.highlight(str, {
+            language: lang
+          }).value + end
+        }
+        catch { return begin + escapeHtml(str) + end }
+      return begin + escapeHtml(str) + end
+    },
+    html: true,
+    breaks: false,
+    linkify: true,
+    typographer: true,
+    quotes: "“”‘’"
+  }).use(emoji)
+    .use(abbr)
+    .use(deflist)
+    .use(sup)
+    .use(sub)
+    .use(footnote)
+    .use(ins)
+    .use(mark)
+    .use(task)
+    .use(iframe)
+    .use(expandable)
+    .use(smartarrows)
+    .use(anchor, {
+      permalink: anchor.permalink.headerLink({ safariReaderFix: true })
+    })
+
+  return converter.render(content)
+}
+
+export default convertMarkdownToHtml
