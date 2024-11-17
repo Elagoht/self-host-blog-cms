0f612e1840f1ce6cf0bbb683ab65bab964e23e8b
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-17 13:52:02 +0300

    feat: update NewBlogPage with breadcrumbs and wrap in Content component; modify package.json for Prisma commands; change translation key from "create" to "new"

--- src/app/(panel)/blogs/new/page.tsx
+++ src/app/(panel)/blogs/new/page.tsx
@@ -2,19 +2,25 @@ import FormBlogAdd from "@/components/forms/FormBlogAdd"
 import { FC } from "react"
 import "@/design/article.css"
 import "@/design/highlight.css"
+import Content from "@/components/layout/Content"
 
 const NewBlogPage: FC = () => {
-  return <FormBlogAdd
-    categories={[{
-      id: "1",
-      name: "TypeScript",
-      slug: "typescript"
-    }, {
-      id: "2",
-      name: "Bicycle",
-      slug: "bicycle"
-    }]}
-  />
+  return <Content breadcrumbs={[
+    { name: "blogs", href: "/blogs" },
+    { name: "new", href: "/blogs/new" }
+  ]}>
+    <FormBlogAdd
+      categories={[{
+        id: "1",
+        name: "TypeScript",
+        slug: "typescript"
+      }, {
+        id: "2",
+        name: "Bicycle",
+        slug: "bicycle"
+      }]}
+    />
+  </Content>
 }
 
 export default NewBlogPage
