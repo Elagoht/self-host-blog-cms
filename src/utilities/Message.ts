2e4a05bce37246f39f981444b2cbcde20c29f6d0
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-18 00:30:32 +0300

    feat: update Message utility to support number arguments in formatting

--- src/utilities/Message.ts
+++ src/utilities/Message.ts
@@ -1,20 +1,20 @@
 export default class Message {
   /**
-   * @param message - The message to format.
+   * @param message - Insert the arguments into the message template.
    * @param args - The arguments to replace in the message.
    * @example Message.format('Hello, {{name}}!', { name: 'John' });
    * @returns 'Hello, John!'
    */
   public static format = (
     message: string,
-    args?: Record<string, string>
+    args?: Record<string, string | number>
   ): string => {
     let formattedMessage = message
     for (
       const key in args
     ) formattedMessage = formattedMessage.replace(
       new RegExp(`{{${key}}}`, "g"),
-      args[key]
+      String(args[key])
     )
     return formattedMessage
   }
