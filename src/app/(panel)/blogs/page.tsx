2a140c4ce22f6bff01344d20e4de534df9b67328
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-19 00:58:24 +0300

    feat: fetch and display blogs on BlogsPage component

--- src/app/(panel)/blogs/page.tsx
+++ src/app/(panel)/blogs/page.tsx
@@ -2,9 +2,12 @@ import Button from "@/components/formElements/Button"
 import Container from "@/components/layout/Container"
 import Content from "@/components/layout/Content"
 import dictionary from "@/i18n"
+import { getBlogs } from "@/services/blog"
 import { FC } from "react"
 
-const BlogsPage: FC<PageComponent> = () => {
+const BlogsPage: FC<PageComponent> = async () => {
+  const blogs = await (await getBlogs()).json()
+
   return <Content breadcrumbs={[
     { name: "blogs", href: "/blogs" }
   ]}>
@@ -18,6 +21,13 @@ const BlogsPage: FC<PageComponent> = () => {
       >
         {dictionary.blogs.main.new}
       </Button>
+
+      <ul>
+        {blogs.map(blog => <li key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+        </li>)}
+      </ul>
     </Container>
   </Content>
 }
