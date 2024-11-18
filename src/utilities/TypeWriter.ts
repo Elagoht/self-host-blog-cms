58f76f79cbf05d2e0ec1d7424a6aa3d2d06b844d
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-19 00:57:31 +0300

    feat: readTime method to calculate reading duration based on text length

--- src/utilities/TypeWriter.ts
+++ src/utilities/TypeWriter.ts
@@ -1,7 +1,19 @@
 import convertMarkdownToHtml from "@/lib/markdown/markdown-it"
 
 class TypeWriter {
+  private static wordsPerMinute = 200
+
   public static markdownToHtml = convertMarkdownToHtml
+
+  public static readTime = (
+    text: string
+  ): number => Math.ceil(
+    text.split(
+      /\s+/
+    ).filter(word =>
+      word.length > 0
+    ).length / TypeWriter.wordsPerMinute
+  )
 }
 
 export default TypeWriter
