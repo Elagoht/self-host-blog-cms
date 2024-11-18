c7bce1cc307c2b01e678cbb2f3f4d39a302fa182
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-19 00:58:15 +0300

    feat: getBlogs and update postBlog to handle FormData for blog submissions

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,5 +1,29 @@
 import ApiCall from "@/utilities/ApiCall"
 
+export const getBlogs = () =>
+  ApiCall.get<BlogResponse[]>("/api/blogs")
+
 export const postBlog = (
   blog: BlogRequest
-) => ApiCall.post("/blogs", blog)
+) => {
+  const formData = new FormData()
+  for (const key in blog) if (
+    blog[key as keyof BlogRequest] !== null
+  ) formData.append(
+    key,
+    String(blog[key as keyof BlogRequest])
+  )
+
+  return ApiCall.post(
+    "/api/blogs",
+    formData
+  )
+}
+
+export const patchBlog = (
+  slug: string,
+  blog: Partial<BlogRequest>
+) => ApiCall.patch(
+  `/api/blogs/${slug}`,
+  blog
+)
