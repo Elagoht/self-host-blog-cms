1a28cd73408edd918ab00020113b592227f5c0c3
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-20 14:28:24 +0300

    fix: delete passphrase requirements

--- src/lib/validation/login.ts
+++ src/lib/validation/login.ts
@@ -3,17 +3,16 @@ import Message from "@/utilities/Message"
 import { object, string } from "yup"
 
 export const loginSchema = object({
-  username: string().required(
-    Message.format(dictionary.form.error.required, {
-      field: dictionary.form.field.username
-    })
-  ),
-  passphrase: string().required(
-    Message.format(dictionary.form.error.required, {
-      field: dictionary.form.field.passphrase
-    })
-  ).min(8, Message.format(dictionary.form.error.min, {
-    field: dictionary.form.field.passphrase,
-    min: "8"
-  }))
+  username: string()
+    .required(
+      Message.format(dictionary.form.error.required, {
+        field: dictionary.form.field.username
+      })
+    ),
+  passphrase: string()
+    .required(
+      Message.format(dictionary.form.error.required, {
+        field: dictionary.form.field.passphrase
+      })
+    )
 })
