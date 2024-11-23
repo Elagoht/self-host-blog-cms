8fc5d3c34c7fbd1cd7fb5c73ad49e39b791efbc2
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-23 15:41:21 +0300

    feat: enhance GET endpoint to handle file retrieval errors with appropriate responses

--- src/app/uploads/[...path]/route.ts
+++ src/app/uploads/[...path]/route.ts
@@ -1,11 +1,26 @@
 import ApiEndpoint from "@/utilities/ApiEndpoint"
-import Bucket from "@/utilities/Bucket"
+import Bucket, { BucketError } from "@/utilities/Bucket"
 
 type Context = Promise<{ path: string[] }>
 
 export const GET = ApiEndpoint<Context>(async (
-  request,
-  context
-) => new Response(await Bucket.getFile((
-  await context.params
-).path.join("/"))))
+  request, context
+) => {
+  try {
+    return new Response(await Bucket.getFile((
+      await context.params
+    ).path.join("/")), {
+      status: 200
+    })
+
+  } catch (error) {
+    if (
+      error instanceof BucketError
+      && error.message === "read"
+    ) return Response.json({
+      message: "notFound"
+    }, { status: 404 })
+
+    throw error
+  }
+})
