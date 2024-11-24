22519466c52776c6ad637ac338475a109c15f62a
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-24 14:12:54 +0300

    feat: added Repositry error handling

--- src/utilities/ApiEndpoint.ts
+++ src/utilities/ApiEndpoint.ts
@@ -3,6 +3,7 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 import { NextRequest } from "next/server"
 import Auth from "./Auth"
 import { FormBodyError } from "./FormBody"
+import RepositoryError from "@/data"
 
 const ApiEndpoint = <T>(
   /**
@@ -24,8 +25,10 @@ const ApiEndpoint = <T>(
   context: { params: T }
 ) => {
     try {
-      if (requireAuth && !await Auth.isValid(
+      if (requireAuth && (!await Auth.isValid(
         Auth.getAccessToken(request.cookies)
+      ) ||
+        !Auth.isTrustedSoftware(request.headers)
       )) return Response.json({
         message: dictionary.api.error.unauthorized
       }, { status: 401 })
@@ -42,6 +45,17 @@ const ApiEndpoint = <T>(
             case "P2002":
               return Response.json({
                 message: dictionary.api.error.uniqueSlug
+              }, { status: 404 })
+            default:
+              return Response.json({
+                message: dictionary.api.error.server
+              }, { status: 500 })
+          }
+        case error instanceof RepositoryError:
+          switch (error.reason) {
+            case "NotFound":
+              return Response.json({
+                message: dictionary.api.error.notFound
               }, { status: 404 })
             default:
               return Response.json({
