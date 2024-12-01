65b34bc218b11bbbe8e3976510a253387a00d327
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-01 08:51:38 +0300

    feat: Expandable component for collapsible content

---
+++ src/components/ui/Expandable.tsx
@@ -0,0 +1,27 @@
+import classNames from "classnames"
+import { FC, ReactNode } from "react"
+
+type ExpandableProps = {
+  expanded: boolean
+  className?: string
+  containerClassName?: string
+  children?: ReactNode
+}
+
+const Expandable: FC<ExpandableProps> = ({
+  expanded, className, children, containerClassName
+}) =>
+  <div className={classNames(
+    "grid transition-all", containerClassName, {
+    "grid-rows-expanded": expanded,
+    "grid-rows-collapsed opacity-0": !expanded
+  })}>
+    <div className={classNames(
+      className,
+      "overflow-hidden"
+    )}>
+      {children}
+    </div>
+  </div>
+
+export default Expandable
