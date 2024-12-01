b694a09c72259d7e82ff7d1891271f923ea3c0ab
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-01 09:51:18 +0300

    feat: Pagination component for improved navigation between pages

---
+++ src/components/ui/Pagination.tsx
@@ -0,0 +1,96 @@
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react"
+import classNames from "classnames"
+import Link from "next/link"
+import QueryString from "qs"
+import { FC, createElement } from "react"
+
+type PaginationProps = {
+  pathname: string
+  totalPages: number
+  searchParams: Record<string, string | undefined>
+  pageParam: string
+  seek?: number
+  seekDirection?: "both" | "forward" | "backward"
+}
+
+const Pagination: FC<PaginationProps> = ({
+  pathname,
+  totalPages,
+  searchParams,
+  pageParam = "page",
+  seek = 2,
+  seekDirection = "both"
+}) => {
+  const currentPage = Number(searchParams[pageParam]) || 1
+
+  const createPageUrl = (page: number) =>
+    `${pathname}${QueryString.stringify({
+      ...searchParams,
+      [pageParam]: page
+    }, {
+      addQueryPrefix: true
+    })}`
+
+  const pageNumbers = (() => {
+    const pages = []
+
+    if (["both", "backward"].includes(seekDirection)) pages.push(
+      ...Array.from({
+        length: currentPage - Math.max(1, currentPage - seek)
+      }, (_, i) => currentPage - i - 1).reverse()
+    )
+
+    pages.push(currentPage)
+
+    if (["both", "forward"].includes(seekDirection)) pages.push(
+      ...Array.from({
+        length: Math.min(totalPages, currentPage + seek) - currentPage
+      }, (_, i) => currentPage + i + 1)
+    )
+
+    return pages
+  })()
+
+  return <div className="flex items-center justify-center space-x-2 mt-4">
+    {createElement(
+      currentPage > 1 ? Link : "div", {
+      href: createPageUrl(currentPage - 1),
+      className: classNames(
+        "h-10 w-10 grid place-items-center text-sm bg-gray-200",
+        "rounded hover:bg-gray-300 dark:bg-neutral-700",
+        "dark:hover:bg-neutral-600", {
+        "opacity-50 cursor-not-allowed": currentPage === 1
+      })
+    }, <IconArrowLeft />)}
+
+
+    {pageNumbers.map((page) =>
+      <Link
+        key={page}
+        href={createPageUrl(page)}
+        className={classNames(
+          "h-10 w-10 grid place-items-center text-sm rounded", {
+          "text-primary-100": page === currentPage,
+          "bg-primary-500 hover:bg-primary-600": page === currentPage,
+          "bg-neutral-200 hover:bg-neutral-300": page !== currentPage,
+          "dark:bg-neutral-700 dark:hover:bg-neutral-600": page !== currentPage
+        })}
+      >
+        {page}
+      </Link>
+    )}
+
+    {createElement(
+      currentPage < totalPages ? Link : "div", {
+      href: createPageUrl(currentPage + 1),
+      className: classNames(
+        "h-10 w-10 grid place-items-center text-sm bg-gray-200",
+        "rounded hover:bg-gray-300 dark:bg-neutral-700",
+        "dark:hover:bg-neutral-600", {
+        "opacity-50 cursor-not-allowed": currentPage === totalPages
+      })
+    }, <IconArrowRight />)}
+  </div>
+}
+
+export default Pagination
