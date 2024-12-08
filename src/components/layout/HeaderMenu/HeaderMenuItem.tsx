ec8082b5aa9802e35c91c7047de92a1f2ab1a976
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-08 15:25:56 +0300

    fix: prefetching logout removes cookies

--- src/components/layout/HeaderMenu/HeaderMenuItem.tsx
+++ src/components/layout/HeaderMenu/HeaderMenuItem.tsx
@@ -11,6 +11,7 @@ const HeaderMenuItem: FC<IHeaderMenuItemProps> = ({
 }) => {
   return <Link
     href={href}
+    prefetch={false}
     className="flex items-center gap-2 p-2 rounded-full transition-all
     hover:bg-primary-500 hover:text-primary-100 hover:shadow"
   >
